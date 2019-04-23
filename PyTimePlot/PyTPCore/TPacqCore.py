c37e45ff69b491cc45028ff7eda39e210ace08f5
jmartinezaguilar <34768462+jmartinezaguilar@users.noreply.github.com>
2019-04-23 13:38:45 +0200

    Add files via upload

---
+++ PyTimePlot/PyTPCore/TPacqCore.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 14:13:45 2019
+
+@author: aguimera
+"""
+
+import PyTPCore.DaqInterface as DaqInt
+import numpy as np
+
+
+# Daq card connections mapping 'Chname':(DCout, ACout)
+aiChannels = {'Ch01': ('ai0', 'ai8'),
+              'Ch02': ('ai1', 'ai9'),
+              'Ch03': ('ai2', 'ai10'),
+              'Ch04': ('ai3', 'ai11'),
+              'Ch05': ('ai4', 'ai12'),
+              'Ch06': ('ai5', 'ai13'),
+              'Ch07': ('ai6', 'ai14'),
+              'Ch08': ('ai7', 'ai15'),
+              'Ch09': ('ai16', 'ai24'),
+              'Ch10': ('ai17', 'ai25'),
+              'Ch11': ('ai18', 'ai26'),
+              'Ch12': ('ai19', 'ai27'),
+              'Ch13': ('ai20', 'ai28'),
+              'Ch14': ('ai21', 'ai29'),
+              'Ch15': ('ai22', 'ai30'),
+              'Ch16': ('ai23', 'ai31'),
+              }
+
+
+##############################################################################
+
+
+class ChannelsConfig():
+
+    # DCChannelIndex[ch] = (index, sortindex)
+    DCChannelIndex = None
+    ACChannelIndex = None
+    ChNamesList = None
+    AnalogInputs = None
+
+    # Events list
+    DCDataDoneEvent = None
+    DCDataEveryNEvent = None
+    ACDataDoneEvent = None
+    ACDataEveryNEvent = None
+    GateDataDoneEvent = None
+    GateDataEveryNEvent = None
+
+    def _InitAnalogInputs(self):
+        print('InitAnalogInputs')
+        self.DCChannelIndex = {}
+        self.ACChannelIndex = {}
+        InChans = []
+        index = 0
+        sortindex = 0
+        for ch in self.ChNamesList:
+            if self.AcqDC:
+                InChans.append(aiChannels[ch][0])
+                self.DCChannelIndex[ch] = (index, sortindex)
+                index += 1
+                print(ch, ' DC -->', aiChannels[ch][0])
+                print('SortIndex ->', self.DCChannelIndex[ch])
+            if self.AcqAC:
+                InChans.append(aiChannels[ch][1])
+                self.ACChannelIndex[ch] = (index, sortindex)
+                index += 1
+                print(ch, ' AC -->', aiChannels[ch][1])
+                print('SortIndex ->', self.ACChannelIndex[ch])
+            sortindex += 1
+
+        if self.GateChannel:
+            self.GateChannelIndex = {self.GateChannel: (index, 0)}
+            InChans.append(self.aiChannels[self.GateChannel][0])
+        else:
+            self.GateChannelIndex = None
+
+        print('Input ai', InChans)
+
+        self.AnalogInputs = DaqInt.ReadAnalog(InChans=InChans)
+        # events linking
+        self.AnalogInputs.EveryNEvent = self.EveryNEventCallBack
+        self.AnalogInputs.DoneEvent = self.DoneEventCallBack
+
+    def _InitAnalogOutputs(self, ChVds, ChVs):
+        print('ChVds ->', ChVds)
+        print('ChVs ->', ChVs)
+        self.VsOut = DaqInt.WriteAnalog((ChVs,))
+        self.VdsOut = DaqInt.WriteAnalog((ChVds,))
+
+    def __init__(self, Channels, GateChannel,
+                 AcqDC=True, AcqAC=True,
+                 ChVds='ao0', ChVs='ao1',
+                 ACGain=1e6, DCGain=10e3):
+        print('InitChannels')
+        self._InitAnalogOutputs(ChVds=ChVds, ChVs=ChVs)
+
+        self.ChNamesList = sorted(Channels)
+        self.GateChannel = GateChannel
+        print self.ChNamesList
+        self.AcqAC = AcqAC
+        self.AcqDC = AcqDC
+        self.ACGain = ACGain
+        self.DCGain = DCGain
+        self._InitAnalogInputs()
+
+    def StartAcquisition(self, Fs, nSamps, Vgs, Vds, **kwargs):
+        print('StartAcquisition')
+        self.SetBias(Vgs=Vgs, Vds=Vds)
+        self.nSamps = nSamps
+        EveryN = len(self.ChNamesList)*nSamps
+        self.AnalogInputs.ReadContData(Fs=Fs,
+                                       EverySamps=EveryN)
+
+    def SetBias(self, Vgs, Vds):
+        print('ChannelsConfig SetBias Vgs ->', Vgs, 'Vds ->', Vds)
+        self.VdsOut.SetVal(Vds)
+        self.VsOut.SetVal(-Vgs)
+        self.BiasVd = Vds-Vgs
+        self.Vgs = Vgs
+        self.Vds = Vds
+
+    def _SortChannels(self, data, SortDict):
+        (samps, inch) = data.shape
+        sData = np.zeros((samps, len(SortDict)))
+        for chn, inds in SortDict.iteritems():
+            sData[:, inds[1]] = data[:, inds[0]]
+
+        return sData
+
+    def EveryNEventCallBack(self, Data):
+        _DCDataEveryNEvent = self.DCDataEveryNEvent
+        _GateDataEveryNEvent = self.GateDataEveryNEvent
+        _ACDataEveryNEvent = self.ACDataEveryNEvent
+
+        if _GateDataEveryNEvent:
+            _GateDataEveryNEvent(self._SortChannels(Data,
+                                                    self.GateChannelIndex))
+        if _DCDataEveryNEvent:
+            _DCDataEveryNEvent(self._SortChannels(Data,
+                                                  self.DCChannelIndex))
+        if _ACDataEveryNEvent:
+            _ACDataEveryNEvent(self._SortChannels(Data,
+                                                  self.ACChannelIndex))
+
+    def DoneEventCallBack(self, Data):
+        print('Done callback')
+
+    def Stop(self):
+        print('Stopppp')
+        self.SetBias(Vgs=0, Vds=0)
+        self.AnalogInputs.StopContData()
+
+
---
+++ PyTimePlot/PyTPCore/TPacqThread.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 12:25:45 2019
+
+@author: aguimera
+"""
+
+from PyQt5 import Qt
+import pyqtgraph.parametertree.parameterTypes as pTypes
+import numpy as np
+import TPacqCore as CoreMod
+import FileModule as FileMod
+
+
+SampSettingConf = ({'title': 'Channels Config',
+                    'name': 'ChsConfig',
+                    'type': 'group',
+                    'children': ({'title': 'Acquire DC',
+                                  'name': 'AcqDC',
+                                  'type': 'bool',
+                                  'value': False},
+                                 {'title': 'Acquire AC',
+                                  'name': 'AcqAC',
+                                  'type': 'bool',
+                                  'value': True},
+                                 {'tittle': 'Channels',
+                                  'name': 'Channels',
+                                  'type': 'group',
+                                  'children': ({'name': 'Ch01',
+                                                'tip': 'Ch01',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch02',
+                                                'tip': 'Ch02',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch03',
+                                                'tip': 'Ch03',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch04',
+                                                'tip': 'Ch04',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch05',
+                                                'tip': 'Ch05',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch06',
+                                                'tip': 'Ch06',
+                                                'type': 'bool',
+                                                'value': True},
+                                               {'name': 'Ch07',
+                                                'tip': 'Ch07',
+                                                'type': 'bool',
+                                                'value': True},
+                                               {'name': 'Ch08',
+                                                'tip': 'Ch08',
+                                                'type': 'bool',
+                                                'value': True},
+                                               {'name': 'Ch09',
+                                                'tip': 'Ch09',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch10',
+                                                'tip': 'Ch10',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch11',
+                                                'tip': 'Ch11',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch12',
+                                                'tip': 'Ch12',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch13',
+                                                'tip': 'Ch13',
+                                                'type': 'bool',
+                                                'value': False},
+                                               {'name': 'Ch14',
+                                                'tip': 'Ch14',
+                                                'type': 'bool',
+                                                'value': True},
+                                               {'name': 'Ch15',
+                                                'tip': 'Ch15',
+                                                'type': 'bool',
+                                                'value': True},
+                                               {'name': 'Ch16',
+                                                'tip': 'Ch16',
+                                                'type': 'bool',
+                                                'value': True},), },
+                                 ), },
+
+                   {'name': 'Sampling Settings',
+                    'type': 'group',
+                    'children': ({'title': 'Sampling Frequency',
+                                  'name': 'Fs',
+                                  'type': 'float',
+                                  'value': 100e3,
+                                  'step': 100,
+                                  'siPrefix': True,
+                                  'suffix': 'Hz'},
+#                                 {'title': 'Interrup Time',
+#                                  'name': 'Inttime',
+#                                  'type': 'float',
+#                                  'value': 0.10,
+#                                  'step': 0.01,
+#                                  'limits': (0.10, 50),
+#                                  'siPrefix': True,
+#                                  'suffix': 's',
+#                                  'readonly': True},
+                                 {'title': 'Fs by Channel',
+                                  'name': 'FsxCh',
+                                  'type': 'float',
+                                  'value': 1e4,
+                                  'step': 100,
+                                  'siPrefix': True,
+                                  'suffix': 'Hz',
+                                  'readonly': True},
+                                 {'title': 'Vds',
+                                  'name': 'Vds',
+                                  'type': 'float',
+                                  'value': 0.05,
+                                  'step': 0.01,
+                                  'limits': (0, 0.1)},
+                                 {'title': 'Vgs',
+                                  'name': 'Vgs',
+                                  'type': 'float',
+                                  'value': 0.1,
+                                  'step': 0.1,
+                                  'limits': (-0.1, 0.5)}, ), }
+                   )
+
+
+###############################################################################
+
+
+class SampSetParam(pTypes.GroupParameter):
+    NewConf = Qt.pyqtSignal()
+
+    Chs = []
+    Acq = {}
+
+    def __init__(self, **kwargs):
+        super(SampSetParam, self).__init__(**kwargs)
+        self.addChildren(SampSettingConf)
+
+        self.SampSet = self.param('Sampling Settings')
+        self.Fs = self.SampSet.param('Fs')
+        self.FsxCh = self.SampSet.param('FsxCh')
+
+        self.ChsConfig = self.param('ChsConfig')
+        self.Channels = self.ChsConfig.param('Channels')
+
+        # Init Settings
+        self.on_Acq_Changed()
+        self.on_Ch_Changed()
+        self.on_Fs_Changed()
+
+        print(self.children())
+        # Signals
+        self.Channels.sigTreeStateChanged.connect(self.on_Ch_Changed)
+        self.ChsConfig.param('AcqAC').sigValueChanged.connect(self.on_Acq_Changed)
+        self.ChsConfig.param('AcqDC').sigValueChanged.connect(self.on_Acq_Changed)
+        self.Fs.sigValueChanged.connect(self.on_Fs_Changed)
+
+    def on_Acq_Changed(self):
+        for p in self.ChsConfig.children():
+            if p.name() is 'AcqAC':
+                self.Acq[p.name()] = p.value()
+            if p.name() is 'AcqDC':
+                self.Acq[p.name()] = p.value()
+        self.NewConf.emit()
+
+    def on_Fs_Changed(self):
+        Ts = 1/self.Fs.value()
+        FsxCh = 1/(Ts*len(self.Columns))
+#        if self.Acq['AcqDC'] and self.Acq['AcqAC'] is True:
+#            FsxCh = FsxCh * 0.5
+#        IntTime = (1/(FsxCh))
+        self.SampSet.param('FsxCh').setValue(FsxCh)
+#        self.SampSet.param('Inttime').setValue(IntTime)
+
+    def on_Ch_Changed(self):
+        self.Chs = []
+        for p in self.Channels.children():
+            if p.value() is True:
+                self.Chs.append(p.name())
+        self.on_Fs_Changed()
+        self.NewConf.emit()
+
+    def GetChannelsNames(self):
+        Ind = 0
+        ChNames = {}
+        acqTys = []
+        for tyn, tyv in self.Acq.items():
+            if tyv:
+                acqTys.append(tyn)
+
+        if 'AcqAC' in acqTys:
+            for Ch in self.Chs:
+                ChNames[Ch + 'AC'] = Ind
+                Ind += 1
+
+        if 'AcqDC' in acqTys:
+            for Ch in self.Chs:
+                ChNames[Ch + 'DC'] = Ind
+                Ind += 1
+        return ChNames
+
+    def GetSampKwargs(self):
+        GenKwargs = {}
+        for p in self.SampSet.children():
+            GenKwargs[p.name()] = p.value()
+        return GenKwargs
+
+    def GetChannelsConfigKwargs(self):
+        ChanKwargs = {}
+        for p in self.ChsConfig.children():
+            if p.name() is 'Channels':
+                ChanKwargs[p.name()] = self.Chs
+            else:
+                ChanKwargs[p.name()] = p.value()
+        print(ChanKwargs)
+        return ChanKwargs
+
+###############################################################################
+
+
+class DataAcquisitionThread(Qt.QThread):
+    NewMuxData = Qt.pyqtSignal()
+
+    def __init__(self, ChannelsConfigKW, SampKw, AvgIndex=5):
+        super(DataAcquisitionThread, self).__init__()
+        self.DaqInterface = CoreMod.ChannelsConfig(**ChannelsConfigKW)
+        self.DaqInterface.DataEveryNEvent = self.NewData
+        self.SampKw = SampKw
+        self.AvgIndex = AvgIndex
+
+    def run(self, *args, **kwargs):
+        self.DaqInterface.StartAcquisition(**self.SampKw)
+        loop = Qt.QEventLoop()
+        loop.exec_()
+
+    def CalcAverage(self, MuxData):
+#        Avg = np.mean(LinesSorted[:,-2:,:], axis=1)
+        return np.mean(MuxData[:, self.AvgIndex:, :], axis=1)
+
+    def NewData(self, aiData):
+#        self.OutData = self.CalcAverage(MuxData)
+        self.aiData = aiData
+        self.NewMuxData.emit()
